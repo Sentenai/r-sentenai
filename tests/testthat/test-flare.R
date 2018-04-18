e4660e2b7e77f83cc2f8091f672fbb75d30c1406
brandly <brandly13@gmail.com>
2018-04-18 15:08:21 -0400

    any_of

--- tests/testthat/test-flare.R
+++ tests/testthat/test-flare.R
@@ -13,6 +13,19 @@ test_that('Or', {
   s <- Stream$new(name = 'S')
   t <- Stream$new(name = 'T')
   real <- json_ast(select()$span(s.x == TRUE || t.x == TRUE))
-  expected <- '{"select":{"expr":"||","args":[{"op":"==","arg":{"type":"boolean","val":true},"type":"span","path":["event","x"],"stream":{"name":"S"}},{"op":"==","arg":{"type":"boolean","val":true},"type":"span","path":["event","x"],"stream":{"name":"T"}}]}}'
+  expected <- '{"select":{"expr":"||","args":[{"op":"==","arg":{"type":"bool","val":true},"type":"span","path":["event","x"],"stream":{"name":"S"}},{"op":"==","arg":{"type":"bool","val":true},"type":"span","path":["event","x"],"stream":{"name":"T"}}]}}'
+  expect_equal(real, expected)
+})
+
+test_that('any_of', {
+  s <- Stream$new(name = 'moose')
+  real <- json_ast(
+    select()$span(any_of(
+      s.x < 0,
+      s.x >= 3.141592653589793,
+      s.b != FALSE
+    ))
+  )
+  expected <- '{"select":{"type":"any","conds":[{"op":"<","arg":{"type":"double","val":0},"type":"span","path":["event","x"],"stream":{"name":"moose"}},{"op":">=","arg":{"type":"double","val":3.14159265358979},"type":"span","path":["event","x"],"stream":{"name":"moose"}},{"op":"!=","arg":{"type":"bool","val":false},"type":"span","path":["event","b"],"stream":{"name":"moose"}}]}}'
   expect_equal(real, expected)
 })
