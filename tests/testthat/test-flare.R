11fea26022d5dfdc231deeedaff9644b65093718
brandly <brandly13@gmail.com>
2018-04-18 15:13:41 -0400

    all_of, during

--- tests/testthat/test-flare.R
+++ tests/testthat/test-flare.R
@@ -29,3 +29,15 @@ test_that('any_of', {
   expected <- '{"select":{"type":"any","conds":[{"op":"<","arg":{"type":"double","val":0},"type":"span","path":["event","x"],"stream":{"name":"moose"}},{"op":">=","arg":{"type":"double","val":3.14159265358979},"type":"span","path":["event","x"],"stream":{"name":"moose"}},{"op":"!=","arg":{"type":"bool","val":false},"type":"span","path":["event","b"],"stream":{"name":"moose"}}]}}'
   expect_equal(real, expected)
 })
+
+test_that('during', {
+  s <- Stream$new(name = 'S')
+  real <- json_ast(
+    select()$span(during(
+      s.foo == 'bar',
+      s.baz > 1.5
+    ))
+  )
+  expected <- '{"select":{"type":"during","conds":[{"op":"==","arg":{"type":"string","val":"bar"},"type":"span","path":["event","foo"],"stream":{"name":"S"}},{"op":">","arg":{"type":"double","val":1.5},"type":"span","path":["event","baz"],"stream":{"name":"S"}}]}}'
+  expect_equal(real, expected)
+})
