bf6c8e07b97deff5dbbd6006bf5ec992d8fbaf49
brandly <brandly13@gmail.com>
2018-04-18 14:47:21 -0400

    test/fix Or

--- tests/testthat/test-flare.R
+++ tests/testthat/test-flare.R
@@ -4,7 +4,15 @@ json_ast <- function(q) {
 
 test_that('Basic select', {
   s <- Stream$new(name = 'S')
-  expected <- '{"select":{"op":"==","arg":{"type":"string","val":"something"},"type":"span","path":["event","key"],"stream":{"name":"S"}}}'
   real <- json_ast(select()$span(s.key == 'something'))
+  expected <- '{"select":{"op":"==","arg":{"type":"string","val":"something"},"type":"span","path":["event","key"],"stream":{"name":"S"}}}'
+  expect_equal(real, expected)
+})
+
+test_that('Or', {
+  s <- Stream$new(name = 'S')
+  t <- Stream$new(name = 'T')
+  real <- json_ast(select()$span(s.x == TRUE || t.x == TRUE))
+  expected <- '{"select":{"expr":"||","args":[{"op":"==","arg":{"type":"boolean","val":true},"type":"span","path":["event","x"],"stream":{"name":"S"}},{"op":"==","arg":{"type":"boolean","val":true},"type":"span","path":["event","x"],"stream":{"name":"T"}}]}}'
   expect_equal(real, expected)
 })
